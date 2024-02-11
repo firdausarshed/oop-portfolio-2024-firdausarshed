ee27cf2a76c26eda6cfff430b4507225a60a4882
firdausarshed <firdausarshed33@gmail.com>
2024-02-11 22:45:25 +0000

    week1 update

---
+++ PersonalDetails/src/detailspackage/DetailsClass.java
@@ -0,0 +1,9 @@
+package detailspackage;
+
+public class DetailsClass {
+
+	public static void main(String[] args) {
+	System.out.println("Name: Firdaus Arshed, Address: 7 Sefton Place, Age: 18, Phone Number: 07386729304");
+	}
+
+}
---
+++ PersonalDetails/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module PersonalDetails {
+}
