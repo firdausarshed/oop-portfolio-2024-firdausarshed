44798faaf36baa9379f1205d75d68a1aba8ac5a1
Admin <admin@Admins-MacBook-Pro.local>
2024-05-11 18:47:47 +0100

    another turtle update

--- AssignmentTurtleGraphicsProgram/jar/GraphicsSystem.java
+++ AssignmentTurtleGraphicsProgram/jar/GraphicsSystem.java
@@ -2,11 +2,14 @@ package jar;
 import javax.swing.*;
 import uk.ac.leedsbeckett.oop.OOPGraphics;
 import java.awt.*;
-
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.ArrayList;
 import static javax.swing.JOptionPane.showMessageDialog;
 
 public class GraphicsSystem extends OOPGraphics
 {
+    private ArrayList<String> commandslist = new ArrayList<String>();
 
     public GraphicsSystem()
     {
@@ -16,73 +19,193 @@ public class GraphicsSystem extends OOPGraphics
             screen.pack();
             screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             screen.setVisible(true);
-            displayMessage("TURTLE GRAPHICS BY FIFI");
+            displayMessage("turtle graphics by fifi");
             penDown();
 
     }
+    public void processCommand(String command){
 
-    public void processCommand(String command)
-    {
+        System.out.println("the command printed was "+command);
 
-        switch (command) {
-            case "penup":
-                penUp();
-                break;
+        if (command.equals("penup"))
+        {
+            penUp();
+            commandslist.add(command);
+        }
 
-            case "about":
-                about();
-                break;
+        else if (command.equals("about"))
+        {
+            about();
+            commandslist.add(command);
+        }
 
-            case "pendown":
-                penDown();
-                break;
+        else if (command.equals("pendown"))
+        {
+            penDown();
+            commandslist.add(command);
+        }
 
-            case "forward":
-                forward(100);
-                break;
+        else if (command.equals("black"))
+        {
+            setPenColour(Color.black);
+            commandslist.add(command);
+        }
 
-            case "backward":
-                forward(-100);
-                break;
+        else if (command.equals("green"))
+        {
+            setPenColour(Color.green);
+            commandslist.add(command);
+        }
 
-            case "turnleft":
-                turnLeft();
-                break;
+        else if (command.equals("red"))
+        {
+            setPenColour(Color.red);
+            commandslist.add(command);
+        }
 
-            case "turnright":
-                turnRight();
-                break;
+        else if (command.equals("white"))
+        {
+            setPenColour(Color.white);
+            commandslist.add(command);
+        }
 
-            case "black":
-                setPenColour(Color.black);
-                break;
+        else if (command.equals("clear"))
+        {
+            clear();
+            commandslist.add(command);
+        }
 
-            case "green":
-                setPenColour(Color.green);
-                break;
+        else if (command.equals("reset"))
+        {
+            reset();
+            commandslist.add(command);
+        }
 
-            case "red":
-                setPenColour(Color.red);
-                break;
+        else if (command.equals("black"))
+        {
+            setPenColour(Color.black);
+            commandslist.add(command);
+        }
 
-            case "white":
-                setPenColour(Color.white);
-                break;
+        else if (command.startsWith("forward"))
+        {
+            String[] spaces = command.split(" ");
+            if (spaces.length == 2)
+            {
+                try
+                {
+                    int distance = Integer.parseInt(spaces[1]);
+                    if (distance <= 0 || distance > 800)
+                    {
+                        JOptionPane.showMessageDialog(null, "enter an appropriate value");
+                    }
+                    else
+                    {
+                        forward(distance);
+                        commandslist.add(command);
+                    }
+                }
+                catch (NumberFormatException forward)
+                {
+                    JOptionPane.showMessageDialog(null, "enter a number");
+                }
+            }
+            else
+            {
+                JOptionPane.showMessageDialog(null, "enter a parameter");
+            }
+        }
 
-            case "clear":
-                clear();
-                break;
-
-            case "reset":
-                reset();
-                break;
-
-            default:
-                showMessageDialog(null, "Invalid Command!");
-                System.out.println("Invalid Command!");
-
+        else if (command.startsWith("backward"))
+        {
+            String[] spaces = command.split(" ");
+            if (spaces.length == 2)
+            {
+                try
+                {
+                    int distance = Integer.parseInt(spaces[1]);
+                    if (distance <= 0 || distance > 800)
+                    {
+                        JOptionPane.showMessageDialog(null, "enter an appropriate value");
+                    }
+                    else
+                    {
+                        forward(-distance);
+                        commandslist.add(command);
+                    }
+                }
+                catch (NumberFormatException forward)
+                {
+                    JOptionPane.showMessageDialog(null, "enter a number");
+                }
+            }
+            else
+            {
+                JOptionPane.showMessageDialog(null, "enter a parameter");
             }
 
         }
 
+        else if (command.startsWith("turnleft"))
+        {
+            String[] spaces = command.split(" ");
+            if (spaces.length == 2)
+            {
+                try
+                {
+                    int degrees = Integer.parseInt(spaces[1]);
+                    if (degrees <0 || degrees > 359)
+                    {
+                        JOptionPane.showMessageDialog(null, "enter an appropriate value");
+                    }
+                    else
+                    {
+                        turnLeft(degrees);
+                        commandslist.add(command);
+                    }
+                }
+                catch (NumberFormatException left)
+                {
+                    JOptionPane.showMessageDialog(null, "enter a number");
+                }
+            }
+            else
+            {
+                JOptionPane.showMessageDialog(null, "enter a parameter");
+            }
+        }
+        else if (command.startsWith("turnright"))
+        {
+            String[] spaces = command.split(" ");
+            if (spaces.length == 2)
+            {
+                try
+                {
+                    int degrees = Integer.parseInt(spaces[1]);
+                    if (degrees <0 || degrees > 359)
+                    {
+                        JOptionPane.showMessageDialog(null, "enter an appropriate value");
+                    }
+                    else
+                    {
+                        turnRight(degrees);
+                        commandslist.add(command);
+                    }
+                }
+                catch (NumberFormatException left)
+                {
+                    JOptionPane.showMessageDialog(null, "enter a number");
+                }
+            }
+            else
+            {
+                JOptionPane.showMessageDialog(null, "enter a parameter");
+            }
+        }
+
+
+
     }
+
+
+}
